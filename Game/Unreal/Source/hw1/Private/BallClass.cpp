ea5348055a77f9b7bffd249486797c3c929b33f3
boming141140 <1269847618zzh@gmail.com>
2023-10-03 23:14:36 -0400

    Adding ball spawning mechanism

---
+++ Game/Unreal/Source/hw1/Private/BallClass.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BallClass.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ABallClass::ABallClass()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMeshFinder(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+	if (SphereMeshFinder.Succeeded())
+	{
+		SphereMesh = SphereMeshFinder.Object;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Shape_Sphere is not found!"));
+	}
+	UStaticMeshComponent* MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetMobility(EComponentMobility::Movable);
+	MeshComponent->SetStaticMesh(SphereMesh);
+
+	// Add the dynamic for hit
+	MeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	MeshComponent->OnComponentHit.AddDynamic(this, &ABallClass::OnHit);
+	owner = nullptr;
+}
+
+// Called when the game starts or when spawned
+void ABallClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABallClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABallClass::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	//if (OtherActor && OtherActor != this)
+	//{
+		// Check if the actor we hit is the player
+		/*APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+
+		// If it is the player, set the owner
+		if (Player)
+		{
+			//Player is holding a ball then dealt damage
+			//ToDo
+			owner = Player;
+		}
+		else  // If it's any other actor, set the owner to nullptr
+		{
+			owner = nullptr;
+		}
+		*/
+	//}
+}
+
---
+++ Game/Unreal/Source/hw1/Private/BallSpawner.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BallSpawner.h"
+#include "UObject/ConstructorHelpers.h"
+#include "EngineUtils.h"
+#include "Engine/StaticMeshActor.h"
+#include "BallClass.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+
+
+//manually hoisting 
+static void RetrieveChuteLocation(FVector& ChuteLocation, UWorld* World);
+
+// Sets default values for this component's properties
+UBallSpawner::UBallSpawner()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	static ConstructorHelpers::FObjectFinder<UPhysicalMaterial> PhysicalMaterialObj(TEXT("PhysicalMaterial'/Game/StarterContent/Materials/Ballbounciness.Ballbounciness'"));
+
+	if (PhysicalMaterialObj.Succeeded())
+	{
+		PhysicalMaterial = PhysicalMaterialObj.Object;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Physical material not found!"));
+	}
+}
+
+
+// Called when the game starts
+void UBallSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+	RetrieveChuteLocation(ChuteLocation, GetWorld());
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &UBallSpawner::SpawnBall, SpawnInterval, true);
+}
+
+// Wrapper for getting ChuteLocation from the Game World
+static void RetrieveChuteLocation(FVector& ChuteLocation, UWorld* World)
+{
+	FString DesiredActorLabel = TEXT("chute");
+	AStaticMeshActor* FoundActor = nullptr;
+
+	for (TActorIterator<AStaticMeshActor> ActorIter(World); ActorIter; ++ActorIter)
+	{
+		if (ActorIter->GetActorLabel() == DesiredActorLabel)
+		{
+			FoundActor = *ActorIter;
+			break;
+		}
+	}
+
+	if (FoundActor)
+	{
+		ChuteLocation = FoundActor->GetActorLocation();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Chute actor not found by label!"));
+	}
+}
+
+
+
+void UBallSpawner::SpawnBall()
+{
+	//Spawn Ball
+	ABallClass* newBall = NewObject<ABallClass>();
+	FVector SpawnLocation = ChuteLocation;  
+	SpawnLocation.Z += 5.0f;
+	FRotator SpawnRotation = FRotator(0.f);     
+
+	AStaticMeshActor* SphereActor = GetWorld()->SpawnActor<AStaticMeshActor>(SpawnLocation, SpawnRotation);
+	if (SphereActor)
+	{
+		UStaticMeshComponent* MeshComp = SphereActor->GetStaticMeshComponent();
+		MeshComp->SetMobility(EComponentMobility::Movable);
+		MeshComp->SetStaticMesh(newBall->SphereMesh);
+		MeshComp->SetSimulatePhysics(true);
+		MeshComp->SetPhysMaterialOverride(PhysicalMaterial);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to spawn the ball!"));
+	}
+	
+	// Increment the counter each time the timer function is called.
+	RepetitionCounter++;
+	if (RepetitionCounter >= MaxRepetitions)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
+	}
+	SpawnedActors.Add(newBall);
+}
+
+
+// Called every frame
+void UBallSpawner::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
